ed265def5f93b29d550842ecd63d00da74681771
Naji Astier <naji@astier.eu>
2019-03-22 13:20:37 +0100

    Extract bankin api request to separate class

---
+++ src/bankin-api.js
@@ -0,0 +1,244 @@
+const { requestFactory, errors, log } = require('cozy-konnector-libs');
+
+const accountTypeMapping = require('./account-type-mapping');
+const operationCategoryMapping = require('./operation-category-mapping');
+
+const request = requestFactory({
+  // the debug mode shows all the details about http request and responses. Very useful for
+  // debugging but very verbose. That is why it is commented out by default
+  debug: false,
+  // activates [cheerio](https://cheerio.js.org/) parsing on each page
+  cheerio: false,
+  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
+  // default in cozy-konnector-libs
+  json: true,
+  // this allows request-promise to keep cookies between requests
+  jar: true
+});
+
+module.exports = class BankinApi {
+  constructor({ clientId, clientSecret, email, password, bankinDevice }) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.email = email;
+    this.password = password;
+    this.bankinDevice = bankinDevice;
+    this.baseUrl = 'https://sync.bankin.com';
+    this.bankinVersion = '2018-06-15';
+    this.accessToken = '';
+    this.banks = [];
+  }
+
+  async init() {
+    log('info', 'Authenticating ...');
+    await this.authenticate();
+    log('info', 'Successfully logged in');
+
+    log('info', 'Fetching banks');
+    await this.fetchBanks();
+    log('info', `Found #${Object.keys(this.banks).length} banks`);
+  }
+
+  authenticate() {
+    const url = `${this.baseUrl}/v2/authenticate`;
+    const qs = {
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      email: this.email,
+      password: this.password
+    };
+
+    const options = {
+      url,
+      qs,
+      method: 'POST',
+      headers: {
+        'bankin-version': this.bankinVersion,
+        'bankin-device': this.bankinDevice
+      }
+    };
+
+    return new Promise(async resolve => {
+      try {
+        const tokens = await request(options);
+
+        this.accessToken = tokens.access_token;
+        resolve(tokens);
+      } catch (error) {
+        throw new Error(errors.LOGIN_FAILED);
+      }
+    });
+  }
+
+  filterOperations(accounts, operations) {
+    const vendorsIds = accounts.map(account => account.vendorId);
+    const operationsIds = [];
+
+    return operations
+      .filter(operation => vendorsIds.indexOf(operation.vendorAccountId) !== -1)
+      .filter(operation => {
+        if (operationsIds.indexOf(operation.vendorId) === -1) {
+          operationsIds.push(operation.vendorId);
+
+          return true;
+        }
+
+        return false;
+      });
+  }
+
+  fetchBanks() {
+    const url = `${this.baseUrl}/v2/banks`;
+    const qs = {
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      limit: 200
+    };
+
+    const options = {
+      url,
+      qs,
+      method: 'GET',
+      headers: {
+        'bankin-version': this.bankinVersion
+      }
+    };
+
+    return new Promise(async resolve => {
+      try {
+        const response = await request(options);
+
+        this.banks = this.formatBanks(response.resources);
+        resolve(this.banks);
+      } catch (error) {
+        throw new Error(errors.VENDOR_DOWN);
+      }
+    });
+  }
+
+  formatBanks(countries) {
+    let banks = {};
+
+    countries.forEach(country => {
+      country.parent_banks.forEach(parentBank => {
+        parentBank.banks.forEach(bank => {
+          banks[bank.id] = bank;
+        });
+      });
+    });
+
+    return banks;
+  }
+
+  fetchAccounts() {
+    const url = `${this.baseUrl}/v2/accounts`;
+    const qs = {
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      limit: 200
+    };
+
+    const options = {
+      url,
+      qs,
+      method: 'GET',
+      headers: {
+        'bankin-version': this.bankinVersion,
+        authorization: `Bearer ${this.accessToken}`
+      }
+    };
+
+    return new Promise(async resolve => {
+      try {
+        const response = await request(options);
+
+        resolve(this.formatAccounts(response.resources));
+      } catch (error) {
+        throw new Error(errors.VENDOR_DOWN);
+      }
+    });
+  }
+
+  formatAccounts(accounts) {
+    return accounts.map(account => {
+      let bank = 'none';
+
+      if (account.bank.id in this.banks) {
+        bank = this.banks[account.bank.id].name;
+      }
+
+      return {
+        label: account.name,
+        institutionLabel: bank,
+        balance: account.balance,
+        type:
+          account.type in accountTypeMapping
+            ? accountTypeMapping[account.type]
+            : 'none',
+        number: account.id,
+        vendorId: account.id
+      };
+    });
+  }
+
+  async fetchOperations(account) {
+    const url = `${this.baseUrl}/v2/accounts/${account.vendorId}/transactions`;
+    const qs = {
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      limit: 200
+    };
+
+    const options = {
+      url,
+      qs,
+      method: 'GET',
+      headers: {
+        'bankin-version': this.bankinVersion,
+        authorization: `Bearer ${this.accessToken}`
+      }
+    };
+    let operations = [];
+    let hasNext = false;
+
+    do {
+      const response = await request(options);
+
+      operations = [
+        ...operations,
+        ...this.formatOperations(response.resources)
+      ];
+      hasNext = false;
+
+      if (response.pagination.next_uri) {
+        hasNext = true;
+        options.url = `${this.baseUrl}${response.pagination.next_uri}`;
+      }
+    } while (hasNext);
+
+    return operations;
+  }
+
+  formatOperations(operations) {
+    return operations.map(operation => {
+      const category =
+        operation.category.id in operationCategoryMapping
+          ? operationCategoryMapping[operation.category.id].cozyCategoryId
+          : 0;
+
+      return {
+        date: new Date(operation.date),
+        label: operation.description,
+        originalLabel: operation.raw_description,
+        type: 'none',
+        automaticCategoryId: category,
+        dateImport: new Date(),
+        dateOperation: new Date(operation.date),
+        currency: operation.currency_code,
+        vendorAccountId: operation.account.id,
+        vendorId: operation.id,
+        amount: operation.amount
+      };
+    });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const {
   BaseKonnector,
-  requestFactory,
-  errors,
   updateOrCreate,
   log,
   cozyClient
@@ -9,22 +7,9 @@ const {
 const doctypes = require('cozy-doctypes');
 const moment = require('moment');
 
-const accountTypeMapping = require('./account-type-mapping');
-const operationCategoryMapping = require('./operation-category-mapping');
+const BankinApi = require('./bankin-api');
 
 const { BankAccount, BankTransaction, BankingReconciliator } = doctypes;
-const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very useful for
-  // debugging but very verbose. That is why it is commented out by default
-  debug: false,
-  // activates [cheerio](https://cheerio.js.org/) parsing on each page
-  cheerio: false,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
-  json: true,
-  // this allows request-promise to keep cookies between requests
-  jar: true
-});
 doctypes.registerClient(cozyClient);
 
 const reconciliator = new BankingReconciliator({
@@ -32,10 +17,6 @@ const reconciliator = new BankingReconciliator({
   BankTransaction
 });
 
-const baseUrl = 'https://sync.bankin.com';
-const bankinVersion = '2018-06-15';
-let accesToken;
-let banks;
 const defaultClientId = process.env.DEFAULT_CLIENT_ID;
 const defaultClientSecret = process.env.DEFAULT_CLIENT_SECRET;
 
@@ -47,17 +28,11 @@ module.exports = new BaseKonnector(start);
 async function start(fields) {
   fields = surchargeFields(fields);
 
-  log('info', 'Authenticating ...');
-  const tokens = await authenticate(fields);
-  accesToken = tokens.access_token;
-  log('info', 'Successfully logged in');
-
-  log('info', 'Fetching banks');
-  banks = await fetchBanks(fields);
-  log('info', `Found #${Object.keys(banks).length} banks`);
+  const bankinApi = new BankinApi(fields);
+  await bankinApi.init();
 
   log('info', 'Fetching the list of accounts');
-  const accounts = await fetchAccounts(fields);
+  const accounts = await bankinApi.fetchAccounts(fields);
   log('info', `Found #${accounts.length} accounts`);
 
   let allOperations = [];
@@ -68,13 +43,13 @@ async function start(fields) {
       'info',
       `Fetching operations of account ${account.vendorId} - ${account.label}`
     );
-    let operations = await fetchOperations(fields, account);
+    let operations = await bankinApi.fetchOperations(account);
     log('info', `Found #${operations.length} operations`);
 
     allOperations = [...allOperations, ...operations];
   }
   log('info', `Found #${allOperations.length} operations before filtering`);
-  allOperations = filterOperations(accounts, allOperations);
+  allOperations = bankinApi.filterOperations(accounts, allOperations);
   log('info', `Found #${allOperations.length} operations after filtering`);
 
   const { accounts: savedAccounts } = await reconciliator.save(
@@ -86,219 +61,15 @@ async function start(fields) {
 }
 
 const surchargeFields = fields => {
-  if (!(typeof fields.client_id === 'string') || fields.client_id.length === 0) {
-    fields.client_id = defaultClientId;
+  if (!(typeof fields.clientId === 'string') || fields.clientId.length === 0) {
+    fields.clientId = defaultClientId;
   }
 
-  if (!(typeof fields.client_secret === 'string') || fields.client_secret.length === 0) {
-    fields.client_secret = defaultClientSecret;
+  if (!(typeof fields.clientSecret === 'string') || fields.clientSecret.length === 0) {
+    fields.clientSecret = defaultClientSecret;
   }
 
   return fields;
-};
-
-const filterOperations = (accounts, operations) => {
-  const vendorsIds = accounts.map(account => account.vendorId);
-  const operationsIds = [];
-
-  return operations
-    .filter(operation => vendorsIds.indexOf(operation.vendorAccountId) !== -1)
-    .filter(operation => {
-      if (operationsIds.indexOf(operation.vendorId) === -1) {
-        operationsIds.push(operation.vendorId);
-
-        return true;
-      }
-
-      return false;
-    });
-};
-
-const authenticate = ({
-  client_id,
-  client_secret,
-  email,
-  password,
-  bankinDevice
-}) => {
-  const url = `${baseUrl}/v2/authenticate`;
-  const qs = {
-    client_id,
-    client_secret,
-    email,
-    password
-  };
-
-  const options = {
-    url,
-    qs,
-    method: 'POST',
-    headers: {
-      'bankin-version': bankinVersion,
-      'bankin-device': bankinDevice
-    }
-  };
-
-  return new Promise(async resolve => {
-    try {
-      const response = await request(options);
-
-      resolve(response);
-    } catch (error) {
-      throw new Error(errors.LOGIN_FAILED);
-    }
-  });
-};
-
-const fetchBanks = ({ client_id, client_secret }) => {
-  const url = `${baseUrl}/v2/banks`;
-  const qs = {
-    client_id,
-    client_secret,
-    limit: 200
-  };
-
-  const options = {
-    url,
-    qs,
-    method: 'GET',
-    headers: {
-      'bankin-version': bankinVersion
-    }
-  };
-
-  return new Promise(async resolve => {
-    try {
-      const response = await request(options);
-
-      resolve(formatBanks(response.resources));
-    } catch (error) {
-      throw new Error(errors.VENDOR_DOWN);
-    }
-  });
-};
-
-const formatBanks = countries => {
-  let banks = {};
-
-  countries.forEach(country => {
-    country.parent_banks.forEach(parentBank => {
-      parentBank.banks.forEach(bank => {
-        banks[bank.id] = bank;
-      });
-    });
-  });
-
-  return banks;
-};
-
-const fetchAccounts = ({ client_id, client_secret }) => {
-  const url = `${baseUrl}/v2/accounts`;
-  const qs = {
-    client_id,
-    client_secret,
-    limit: 200
-  };
-
-  const options = {
-    url,
-    qs,
-    method: 'GET',
-    headers: {
-      'bankin-version': bankinVersion,
-      authorization: `Bearer ${accesToken}`
-    }
-  };
-
-  return new Promise(async resolve => {
-    try {
-      const response = await request(options);
-
-      resolve(formatAccounts(response.resources));
-    } catch (error) {
-      throw new Error(errors.VENDOR_DOWN);
-    }
-  });
-};
-
-const formatAccounts = accounts => {
-  return accounts.map(account => {
-    let bank = 'none';
-
-    if (account.bank.id in banks) {
-      bank = banks[account.bank.id].name;
-    }
-
-    return {
-      label: account.name,
-      institutionLabel: bank,
-      balance: account.balance,
-      type:
-        account.type in accountTypeMapping
-          ? accountTypeMapping[account.type]
-          : 'none',
-      number: account.id,
-      vendorId: account.id
-    };
-  });
-};
-
-const fetchOperations = async ({ client_id, client_secret }, account) => {
-  const url = `${baseUrl}/v2/accounts/${account.vendorId}/transactions`;
-  const qs = {
-    client_id,
-    client_secret,
-    limit: 200
-  };
-
-  const options = {
-    url,
-    qs,
-    method: 'GET',
-    headers: {
-      'bankin-version': bankinVersion,
-      authorization: `Bearer ${accesToken}`
-    }
-  };
-  let operations = [];
-  let hasNext = false;
-
-  do {
-    const response = await request(options);
-
-    operations = [...operations, ...formatOperations(response.resources)];
-    hasNext = false;
-
-    if (response.pagination.next_uri) {
-      hasNext = true;
-      options.url = `${baseUrl}${response.pagination.next_uri}`;
-    }
-  } while (hasNext);
-
-  return operations;
-};
-
-const formatOperations = operations => {
-  return operations.map(operation => {
-    const category =
-      operation.category.id in operationCategoryMapping
-        ? operationCategoryMapping[operation.category.id].cozyCategoryId
-        : 0;
-
-    return {
-      date: new Date(operation.date),
-      label: operation.description,
-      originalLabel: operation.raw_description,
-      type: 'none',
-      automaticCategoryId: category,
-      dateImport: new Date(),
-      dateOperation: new Date(operation.date),
-      currency: operation.currency_code,
-      vendorAccountId: operation.account.id,
-      vendorId: operation.id,
-      amount: operation.amount
-    };
-  });
 };
 
 const fetchBalances = accounts => {
